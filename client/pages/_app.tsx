02e3b082ec6fae1d0913b0fb6814cc00471e208c
Tugkan Kibar <tugkan@orangebandits.com>
2022-07-16 20:29:27 +0200

    Adds rainbow to connect a wallet

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,21 +1,51 @@
 import '../styles/globals.css';
+import '@rainbow-me/rainbowkit/styles.css';
+
 import type { AppProps } from 'next/app';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
 import { Sidebar } from '../components/layout/sidebar';
 import { Header } from '../components/layout/header';
 import { SuggestionsBar } from '../components/layout/suggestions-bar';
 
+const { chains, provider } = configureChains(
+  [chain.polygonMumbai],
+  [
+    alchemyProvider({ alchemyId: process.env.NEXT_PUBLIC_ALCHEMY_ID }),
+    publicProvider(),
+  ],
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'Chain Me Up',
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="fixed flex w-full h-full">
-      <Header />
-      <div className="container flex flex-col flex-1 mx-auto sm:flex-row">
-        <main className="flex-1 overflow-auto border-gray-50 border-x-2">
-          <Component {...pageProps} />
-        </main>
-        <Sidebar />
-        <SuggestionsBar />
-      </div>
-    </div>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <div className="fixed flex w-full h-full">
+          <Header />
+          <div className="container flex flex-col flex-1 mx-auto sm:flex-row">
+            <main className="flex-1 overflow-auto border-gray-50 border-x-2">
+              <Component {...pageProps} />
+            </main>
+            <Sidebar />
+            <SuggestionsBar />
+          </div>
+        </div>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
 
