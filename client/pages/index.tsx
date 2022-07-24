50af70410bab7db864f8e5e31a5f54bbc08bcd4d
Juan C. Andreu <andreujuanc@gmail.com>
2022-07-24 14:35:53 +0000

    logos

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from 'next';
 import dynamic from 'next/dynamic';
+import NextHead from 'next/head';
 
 const WalletManagement = dynamic(
   async () => {
@@ -14,11 +15,14 @@ const WalletManagement = dynamic(
 
 const Login: NextPage = () => {
   return (
-    <main className="fixed flex items-center justify-center w-full h-full bg-gradient-to-br from-primary-600 to-primary-50">
+    <main className="fixed flex flex-col items-center justify-center w-full h-full bg-gradient-to-br from-primary-600 to-primary-50">
+      <NextHead>
+        <title>OnlyFrens</title>
+      </NextHead>
       <section className="items-center justify-center max-w-sm p-12 bg-white rounded-lg shadow-xl">
-        <h1 className="mb-4 text-xl font-bold text-center text-primary-500">
-          Chain Me Up
-        </h1>
+        <div  className="px-6 py-2 bg-primary-50 flex items-center justify-center rounded-lg padding-lg">
+          <img src="/images/logo/logo.svg" alt='Logo'  />
+        </div>
         <WalletManagement />
       </section>
     </main>
